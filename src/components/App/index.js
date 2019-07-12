895aae44a63ea8aa738d173d9da47f4ef1c5fd8c
Josh Merritt <merrittjosh@gmail.com>
2019-07-12 08:26:30 -0700

    Added Scorecard Component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,6 +9,7 @@ import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
+import Scorecard from '../Scorecard';
 
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
@@ -31,6 +32,7 @@ const App = () => (
       <Route exact path={ROUTES.HOME} component={HomePage} />
       <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+      <Route exact path={ROUTES.SCORECARD} component={Scorecard} />
     </div>
   </Router>
 );
