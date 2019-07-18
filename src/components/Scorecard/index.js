8b11f4c0dc7d77df53bce3cbd5fc46c8b290c895
Josh Merritt <merrittjosh@gmail.com>
2019-07-18 10:59:07 -0700

    Make handicap customized to each round

--- src/components/Scorecard/index.js
+++ src/components/Scorecard/index.js
@@ -71,9 +71,7 @@ class Scorecard extends Component {
       tempUser.holes = holes;
       playersTemp.push(tempUser);
     };
-    console.log('playersTemp', playersTemp);
     let scorecardTemp = this.props.firebase.scorecard(this.state.scorecard.uid);
-    console.log(scorecardTemp);
     scorecardTemp.set({
       players: playersTemp,
     });
@@ -87,50 +85,82 @@ class Scorecard extends Component {
   //   console.log(this.state.scorecard);
   //   return (
   //     <input>this</input>
+  //    {{this.state.scorecard.players[player.uid].holes.map((hole)=> {
+  //          <input className="scores" name="scores" value={hole} type='number'/>
+  //    })}}
   //   )
   // }
 
-  // onChange(event) => {
-  //   const value = event.target.value;
-  //   const player = "Josh Merritt"
-  //   this.setState(() => {scorecard.player.handicap})
-  // }
+  onHandicapChange = event => {
+    const tempId = event.target.id;
+    let players = this.state.scorecard.players;
+    let playersIndex = '';
+    let player = players.find(function(item, index) {
+      playersIndex = index;
+      return tempId === item.uid
+    });
+    player.handicap = event.target.value;
+    players[playersIndex] = player;
+    this.setState({players: players});
+  };
 
-  onTextChanged = (event) => {
-    const value = event.target.value;
-    let suggestions = [];
-    if(value.length > 0) {
-      const regex = new RegExp(`^${value}`, 'i');
-      suggestions = this.state.users.sort().filter(item => regex.test(item));
-    }
-      this.setState(() =>  ({ suggestions, text: value }));     
+  // onTextChanged = (event) => {
+  //   const value = event.target.value;
+  //   let suggestions = [];
+  //   if(value.length > 0) {
+  //     const regex = new RegExp(`^${value}`, 'i');
+  //     suggestions = this.state.users.sort().filter(item => regex.test(item));
+  //   }
+  //     this.setState(() =>  ({ suggestions, text: value }));     
       
-  }
+  // }
 
   render() {
     if(this.state.loading){
       return <h2>Loading...</h2>
     };
     return (
-      <div>
-      
-        <h1>Scorecard</h1>
-        <table>
+      <div className="scorecard">
+       <h1>Scorecard</h1>
+        <table className="holes">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Handicap</th>
+              <th>Hole: 1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+              <th>7</th>
+              <th>8</th>
+              <th>9</th>
+              <th>10</th>
+              <th>11</th>
+              <th>12</th>
+              <th>13</th>
+              <th>14</th>
+              <th>15</th>
+              <th>16</th>
+              <th>17</th>
+              <th>18</th>
+            </tr>
+          </thead>
           <tbody>
             {this.state.scorecard.players.map((player) => {
               return (
                 <tr key={player.uid}>
-                  {player.username} 
-                  <input className="handicap" name="handicap" value={player.handicap} onChange={this.onChange} type="number"/>
-                  {/* {this.state.scorecard.players[player.uid].holes.map((hole)=> {
-                    <input className="scores" name="scores" value={hole} type='number'/>
-                  })} */}
+                  <td>{player.username}</td>
+                  <td>
+                    <input className="handicap" id={player.uid} name={player.name} value={player.handicap} onChange={this.onHandicapChange} type="number"/>
+                  </td>
                 </tr>
               )
             })}
           </tbody>
         </table>
-        <button onClick={this.updatePlayerData}> Update Player Data </button>
+        <button onClick={this.updatePlayerData}>Update Player Data</button>
       </div>
     );
   }
