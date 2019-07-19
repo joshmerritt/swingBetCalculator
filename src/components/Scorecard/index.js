fdf2c2ef3718549309434f268cf5ad9af8f32848
Josh Merritt <merrittjosh@gmail.com>
2019-07-19 09:26:39 -0700

    Save scorecard to database

--- src/components/Scorecard/index.js
+++ src/components/Scorecard/index.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-
+import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
 
+/* 
+  Creates a scorecard table for the most recent created scorecard.
+  
+
+*/
 
 class Scorecard extends Component {
   constructor(props) {
-    super(props);
-
-    this.state = {
-      users: [],
-      players: [],
-      scorecard: [],
-      course: [],
-      loading: true,
-    };
+  super(props);
+  this.state = {
+    users: [],
+    players: [],
+    scorecard: [],
+    course: [],
+    loading: true,
+  };
   this.updatePlayerData = this.updatePlayerData.bind(this);
   this.onHandicapChange = this.onHandicapChange.bind(this);
   this.onScoreChange = this.onScoreChange.bind(this);
+  this.saveScorecard = this.saveScorecard.bind(this);
   }
 
+  /* Load user list, most recent scorecard, and course */
   componentDidMount() {
       this.props.firebase.users().on('value', snapshot => {
         const usersObject = snapshot.val();
@@ -41,7 +47,7 @@ class Scorecard extends Component {
           scorecard: lastScorecard,
           players: lastScorecard.players,
         });
-        if(lastScorecard.players[0].uid) {
+        if(lastScorecard.players && lastScorecard.players[0].uid && lastScorecard.players[0].holes) {
           this.setState({loading: false});
         }
       });
@@ -58,6 +64,7 @@ class Scorecard extends Component {
       });
   }
 
+  /* Create Holes array to hold each players scores */
   updatePlayerData() {
     let playersTemp = [];
     let holes = [
@@ -99,7 +106,7 @@ class Scorecard extends Component {
       players[playersIndex] = player;
       this.setState({players: players});
     };
-  };
+  }
 
   onScoreChange = event => {
     const value = event.target.value;
@@ -128,72 +135,104 @@ class Scorecard extends Component {
       players[playersIndex] = player;
       this.setState({players: players});
     };
-  };
+  }
+
+  saveScorecard(event) {
+    event.preventDefault();
+    const currentScorecardKey = this.state.scorecard.uid;
+    console.log('currentScorecardKey', currentScorecardKey);
+    let newRecord = this.props.firebase.db.ref('scorecards/' + currentScorecardKey);
+    console.log('scorecard Record', newRecord);
+    // let newItem = {
+    //   dateOfRound: today,
+    //   players: this.state.playerList,
+    // };
+    // this.setState({
+    //   scorecard: newRecord,
+    // });
+    console.log('this.state', this.state);
+    newRecord.set(this.state.scorecard.players);
+    this.props.history.push(ROUTES.HOME);
+  
+  }
 
   render() {
     if(this.state.loading){
+      if(this.state.scorecard.players && !this.state.scorecard.players[0].holes) {
+        return (
+          <div>
+            <h2>Loading...</h2>
+            <button onClick={this.updatePlayerData}>Initialize Scorecard</button>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <h2>Fetching scorecard</h2>
+          </div>
+        )
+      }
+    } else {
       return (
-        <div>
-          <h2>Loading...</h2>
-          <button onClick={this.updatePlayerData}>Update Player Data</button>
+        <div className="scorecard">
+         <h1>Scorecard</h1>
+          <table className="holes">
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Handicap</th>
+                <th>Swinger?</th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+                <th>7</th>
+                <th>8</th>
+                <th>9</th>
+                <th>10</th>
+                <th>11</th>
+                <th>12</th>
+                <th>13</th>
+                <th>14</th>
+                <th>15</th>
+                <th>16</th>
+                <th>17</th>
+                <th>18</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.scorecard.players.map((player) => {
+                return (
+                  <tr key={player.uid}>
+                    <td>{player.username}</td>
+                    <td>
+                      <input className="handicap" id={player.uid} name={player.name} value={player.handicap} onChange={this.onHandicapChange} type="number"/>
+                    </td>
+                    <td>
+                      <input type="checkbox" name={player.username} value="false"/>
+                    </td>
+                    {player.holes.map((item, index) => {
+                      let playerHole = player.username + " " + item.name;
+                      return (
+                        <td key={playerHole}>
+                          <input className="hole" id={playerHole} name={player.username} value={item.score} onChange={this.onScoreChange} type="number"/>
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+          <div>
+            <br/> <br/>
+            <button className="saveButton" onClick={this.saveScorecard}>Save Scorecard</button>
+          </div>    
         </div>
-      )
-    };
-    return (
-      <div className="scorecard">
-       <h1>Scorecard</h1>
-        <table className="holes">
-          <thead>
-            <tr>
-              <th>Player</th>
-              <th>Handicap</th>
-              <th>Swinger?</th>
-              <th>1</th>
-              <th>2</th>
-              <th>3</th>
-              <th>4</th>
-              <th>5</th>
-              <th>6</th>
-              <th>7</th>
-              <th>8</th>
-              <th>9</th>
-              <th>10</th>
-              <th>11</th>
-              <th>12</th>
-              <th>13</th>
-              <th>14</th>
-              <th>15</th>
-              <th>16</th>
-              <th>17</th>
-              <th>18</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.scorecard.players.map((player) => {
-              return (
-                <tr key={player.uid}>
-                  <td>{player.username}</td>
-                  <td>
-                    <input className="handicap" id={player.uid} name={player.name} value={player.handicap} onChange={this.onHandicapChange} type="number"/>
-                  </td>
-                  <td>
-                    <input type="checkbox" name={player.username} value="false"/>
-                  </td>
-                  {player.holes.map((item, index) => {
-                    let playerHole = player.username + " " + item.name;
-                    return (
-                      <td key={playerHole}>
-                        <input className="hole" id={playerHole} name={player.username} value={item.score} onChange={this.onScoreChange} type="number"/>
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>       
-      </div>
-    );
+      );
+    }
   }
 }
 
